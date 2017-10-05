957772781316fb757d3e61ff957995c2fa84ee0e
Ben Parsons <9parsonsb@gmail.com>
2017-10-05 12:27:37 +1100

    Started command identification Added Enums for common command types Added Enum resolvers for common commands (EPLType, EPLForm, EPLFormat, EPLReverseType)

--- EPL2Bitmap/libEPL2Bitmap/EPL2Bitmap.cs
+++ EPL2Bitmap/libEPL2Bitmap/EPL2Bitmap.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Linq;
 
 namespace libEPL2Bitmap
 {
@@ -7,11 +9,77 @@ namespace libEPL2Bitmap
         Bitmap ConvertFromString(string EPL);
     }
 
-    public class EPL2Bitmap : IEPL2Bitmap
+    public partial class EPL2Bitmap : IEPL2Bitmap
     {
         public Bitmap ConvertFromString(string EPL)
         {
-            throw new System.NotImplementedException();
+            var lines = EPL.Split(Environment.NewLine.ToCharArray());
+            Bitmap bmp = null;
+            foreach (var line in lines)
+            {
+                var type = GetEPLType(line.Substring(0, 1).ToCharArray()[0]);
+
+                switch (type)
+                {
+                    case EPLTypeEnum.String:
+                        RenderString(line, ref bmp);
+                        break;
+                    case EPLTypeEnum.Barcode:
+                        RenderBarcode(line, ref bmp);
+                        break;
+                    case EPLTypeEnum.NewLine:
+                        ApplyNewLine(line, ref bmp);
+                        break;
+                    case EPLTypeEnum.Setting: // If a setting is defined midway, it only applies to lines after it
+                        ApplySetting(line);
+                        break;
+                    case EPLTypeEnum.Format:
+                        ApplyFormat(line);
+                        break;
+                    case EPLTypeEnum.Quantity: // What do we do with this?
+                        SetQuantity(line);
+                        break;
+                    case EPLTypeEnum.Unknown:
+                        var num = lines.ToList().IndexOf(line);
+                        throw new Exception($"unknown character on line: {num}:{Environment.NewLine}{line}");
+                }
+            }
+            // ReSharper disable once ExpressionIsAlwaysNull
+            return bmp;
         }
+
+        private static void SetQuantity(string line)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ApplyFormat(string line)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ApplyNewLine(string line, ref Bitmap bmp)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ApplySetting(string line)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void RenderBarcode(string line, ref Bitmap bmp)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void RenderString(string line, ref Bitmap bmp)
+        {
+            throw new NotImplementedException();
+        }
+
+        
     }
+
+
 }
---
+++ EPL2Bitmap/libEPL2Bitmap/EPLTypeEnum.cs
@@ -0,0 +1,91 @@
+﻿namespace libEPL2Bitmap
+{
+
+    public partial class EPL2Bitmap
+    {
+        private enum EPLTypeEnum
+        {
+            String, // A (ASCII TEXT) String line
+            Barcode, // B Barcode Line / b other image (e.g. QR code)
+            Setting, // D / O
+            NewLine, // N
+            Format, // Q / q
+            Quantity, // P
+            Form, // F Manipulate stored Form (functions) SortedList<string,[string|bmp]>?
+            Unknown = -1
+        }
+
+        private enum EPLFormFunctions
+        {
+            Information, // I List stored forms and information
+            Store, // S Start a stored function
+            Retrieve, // R Insert inline
+            Delete,
+            End, // End of stored function
+            Unknown = -1
+        }
+        
+        private enum EPLFormatEnum
+        {
+            
+            Width, // X
+            /// <summary>
+            /// how long the label is (hiehgt)
+            /// </summary>
+            Length, // Y (Height)
+            Gap, // Gap between each label
+            /// <summary>
+            /// How dark (usually how much heat to apply)
+            /// </summary>
+            Heat,
+            Speed, // I dont think this matters
+            Unknown = -1
+        }
+
+        private enum EPLReverseTypeEnum
+        {
+            WhiteOnBlack, // R
+            BlackOnWhite, // N
+            Unknown = -1
+        }
+        private static EPLTypeEnum GetEPLType(char type)
+        {
+            switch (type)
+            {
+                case 'A':
+                    return EPLTypeEnum.String;
+                case 'B':
+                    return EPLTypeEnum.Barcode;
+                case 'D':
+                case 'O':
+                    return EPLTypeEnum.Setting;
+                case 'N':
+                    return EPLTypeEnum.NewLine;
+                case 'Q':
+                case 'q':
+                    return EPLTypeEnum.Format;
+                case 'P':
+                    return EPLTypeEnum.Quantity;
+                case 'F':
+                    return EPLTypeEnum.Form;
+                default:
+                    return EPLTypeEnum.Unknown;
+            }
+        }
+
+        private static EPLReverseTypeEnum GetEPLReverseType(char type)
+        {
+            switch (type)
+            {
+                case 'N':
+                    return EPLReverseTypeEnum.BlackOnWhite;
+                case 'R':
+                    return EPLReverseTypeEnum.WhiteOnBlack;
+                default:
+                    return EPLReverseTypeEnum.Unknown;
+            }
+        }
+    }
+
+
+}
